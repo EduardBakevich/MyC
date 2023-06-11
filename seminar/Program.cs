eb9f6effa37b874b580063d2698fcaf2ca9e234b
EduardBakevich <ed.bakevich@gmail.com>
2023-06-11 12:30:42 +0300

    sem1

---
+++ seminar/Program.cs
@@ -0,0 +1,52 @@
+﻿// Console.Write("Введите число, квадрат которого вы хотите увидеть -> ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int quad = num * num;
+// Console.WriteLine($"Квадрат числа {num} => {quad}");
+
+//Задача2
+
+// Console.Clear();
+// Console.Write("Введите первое число -> ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите второе число -> ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// int quad = num1 * num1;
+
+// if (num2 == quad)
+// {
+//     Console.WriteLine($"ОГО!! {num1} — квадрат {num2}");
+// }
+// else
+// {
+//     Console.WriteLine($" {num1} не является квадратом {num2}");
+// }
+
+// Задача3
+
+// Console.Clear();
+// Console.Write("Введите любое целое число N -> ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// int current = n * (-1);
+
+// while (current <= n)
+// {
+//     Console.Write(current + " ");
+//     current ++;
+// }
+
+// Задача 4
+
+Console.Clear();
+Console.Write("Введите любое трехзначное число -> ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 100 && num <= 999)
+{
+    Console.WriteLine(num%10);
+}
+else
+{
+    Console.WriteLine("вы ввели не трехначное число!!!");
+}
