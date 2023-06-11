afa421dbf69cdcb4936c90624f7772ca9a29e7f0
EduardBakevich <ed.bakevich@gmail.com>
2023-06-11 16:57:31 +0300

    home1

---
+++ homeWorks/1/Program.cs
@@ -0,0 +1,80 @@
+﻿// Поиск наибольшего из двух цифр
+// Console.Clear();
+// Console.Write("Введите первое число -> ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число -> ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// int max = num1;
+
+// if (num2 > max)
+// {
+//     max = num2;
+// }
+
+// Console.WriteLine($" наибольшее число из введенных — {max}");
+
+
+
+// Поиск наибольшего из трех цифр
+// Console.Clear();
+// Console.Write("Введите первое число -> ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число -> ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите третье число -> ");
+// int num3 = Convert.ToInt32(Console.ReadLine());
+
+// int max = num1;
+
+// if (num2 > max)
+// {
+//     max = num2;
+// }
+
+// if (num3 > max)
+// {
+//     max = num3;
+// }
+
+// Console.WriteLine($" наибольшее число из введенных — {max}");
+
+
+// Чет или нечет?
+// Console.Clear();
+// Console.Write("Введите число -> ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if (num%2 == 0)
+//  {
+//      Console.WriteLine("Введенное вами число — четное");
+// }
+// else
+// {
+//      Console.WriteLine("Введенное вами число — нечетное");
+// }
+
+
+// Console.Clear();
+// Console.Write("Введите любое целое положительное число N -> ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// if (n ==  1)
+// {
+//     Console.WriteLine($"В последовательности от 0 до {n} нет четных чисел");
+// }
+
+// int current = 2;
+
+// while (current <= n)
+// {
+//     if (current % 2 == 0)
+//     {
+//     Console.Write(current + " ");
+//     current ++;
+//     }
+// else
+//     {
+//      current ++;
+//     }
+// }
