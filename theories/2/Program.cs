892d237c9e6396257ba04e402942d9f5deb9bd7f
EduardBakevich <ed.bakevich@gmail.com>
2023-06-12 18:39:05 +0300

    Lekcya2.1

--- theories/2/Program.cs
+++ theories/2/Program.cs
@@ -70,21 +70,56 @@
 //     index++;
 // }
 
-// Способы записи значений массивов
-Console.Clear ();
-// создание нового массива из 10 элементов. По умолчанию он заполнится
-// нулями, поэтому надо прописать метод/функцию
-void FillArray(int[] collection)
-{
-    int length = collection.Length;
-    int index = 0;
-    while (index < length)
-    {
-        collection[index] = new Random().Next(1, 10);
-        index++;
-    }
-}
+// // Способы записи значений массивов
+// Console.Clear ();
+// // создание нового массива из 10 элементов. По умолчанию он заполнится
+// // нулями, поэтому надо прописать метод/функцию void. Ее особенность
+// // в том, что она не возвращает никаких значений
+// void FillArray(int[] collection)
+// {
+//     int length = collection.Length;
+//     int index = 0;
+//     while (index < length)
+//     {
+//         collection[index] = new Random().Next(1, 10);
+//         index++;
+//     }
+// }
 
-void PrintArray(int[] col)
-{}
-int[] array = new int[10]; 
+// void PrintArray(int[] col)
+// {
+//     int count = col.Length;
+//     int position = 0;
+//     while (position < count)
+//     {
+//         Console.WriteLine(col[position]);
+//         position++;
+//     }
+// }
+
+// int IndexOff(int[] collection, int find)
+// {
+//     int count = collection.Length;
+//     int index = 0;
+//     int position = -1;
+
+//     while (index < count)
+//     {
+//         if(collection[index] == find)
+//         {
+//             position = index;
+//             break;
+//         }
+//         index++;
+//     }
+//     return position;
+// } 
+
+// int[] array = new int[10];
+
+// FillArray(array);
+// PrintArray(array);
+// Console.WriteLine();
+
+// int pos = IndexOff(array, 4);
+// Console.WriteLine(pos);
