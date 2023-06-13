1397cb9d3be04c11642e006869e0cdcc1cb5a19a
EduardBakevich <ed.bakevich@gmail.com>
2023-06-13 14:38:10 +0300

    day2

---
+++ homeWorks/2/Program.cs
@@ -0,0 +1,59 @@
+﻿// Программа принимает на вход трехзначное число и показывает 
+// вторую цифру этого числа
+// Console.Clear();
+
+// void Dozens(int num)
+// {
+//     if (num >= 100 && num <= 999)
+//     {
+//     System.Console.WriteLine($"число десятков -> {(num / 10) % 10}");
+//     }
+//     else
+//     {
+//         System.Console.WriteLine($"число {num} не трехзначное");
+//     }
+// }
+// Console.Write ("Введите трехзначное число -> ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// Dozens(num);
+
+
+// программа выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.
+
+// Console.Clear();
+
+// void Hundred(int num)
+// {
+//     if (num >= 100)
+//     {
+//     System.Console.WriteLine($"число сотен -> {(num / 100) % 10}");
+//     }
+//     else
+//     {
+//         System.Console.WriteLine($"число {num} одно- или двухзначное");
+//     }
+// }
+// Console.Write ("Введите число -> ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// Hundred(num);
+
+
+// Программа принимает на вход цифру, обозначающую день недели, и
+// проверяет, является ли этот день выходным
+
+// Console.Clear();
+
+// bool Week (int num)
+// {
+//     if (num > 5) return true;
+//     else return false;
+// }
+
+// Console.Write ("Хотите узнать: нужный вам день недели выходной или нет? Введите его порядковый номер от 1 (понедельник) до 7 (воскресенье) -> ");
+
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// System.Console.WriteLine(Week(num));
--- seminars/2/Program.cs
+++ seminars/2/Program.cs
@@ -1,2 +1,77 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// // Кратно ли 1е число, 2му
+// Console.Clear();
+// void CheckEven(int num1, int num2)
+// {
+//     if (num1 % num2 == 0)
+//     {
+//         Console.WriteLine($"Число {num1} кратно {num2} ");
+//     }
+//     else
+//     {
+//         Console.WriteLine($"Число {num1} не кратно {num2}. Остаток от деления — {num1 % num2} ");
+//     }
+// }
+
+// Console.Write("Введите первое число -> ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите второе число -> ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// CheckEven(num1, num2);
+
+
+// // Кратно ли одно число, одновременно 7 и 23
+// Console.Clear();
+
+// bool CheckEven(int num)
+// {
+//     if(num % 7 == 0 && num % 23 == 0) return true;
+//     else return false;
+// }
+
+// Console.Write ("Введите число -> ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// System.Console.WriteLine(CheckEven(num));
+
+
+// Программа выводит трехзначное число и удаляет вторую цифру
+// этого числа
+// Console.Clear();
+
+// ﻿int num = new Random().Next(100, 999);
+
+// Console.WriteLine(num);
+
+// System.Console.WriteLine($"{num / 10 - (num / 10) % 10 + num % 10}");
+
+
+// Программа принимает два числа и проверяет, является ли
+// одно число квадратом другого
+
+// Console.Clear();
+
+// void Squared(int num1, int num2)
+// {
+//     if (num1 == num2*num2) 
+//     {
+//         Console.WriteLine($"{num2} является квадратом {num1}");
+//     }
+//     if (num2 == num1*num1)
+//     {
+//         Console.WriteLine($"{num1} является квадратом {num2}");
+//     }
+//     else
+//     {
+//         Console.WriteLine($"{num1} и {num2} не являются квадратом друг друга");
+//     }
+// }
+
+// Console.Write("Введите первое число -> ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите второе число -> ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// Squared(num1, num2);
