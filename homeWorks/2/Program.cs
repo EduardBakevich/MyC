8b98f06c32eff3a50bb9fdaa621fef779db543a4
EduardBakevich <ed.bakevich@gmail.com>
2023-06-18 19:34:11 +0300

    home2

---
+++ homeWorks/2/Program.cs
@@ -0,0 +1,18 @@
+﻿// /* Проверка: является ли пятизначное число палиндромом?*/
+// Console.Clear();
+// bool result (int num)
+// {
+//     if (num / 10000 == num % 10 && num % 10000 / 1000 == num % 100 / 10)
+//     {
+//     return true;
+//     }
+// return false;
+// }
+// Console.Write ( " Введите пятизначное число => " );
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if (num < 10000 && num > 99999)
+// {
+//     Console.WriteLine("Ошибка ввода: условия не соблюдены");
+// }
+// Console.WriteLine($"Число {num} — палиндром? => {result(num)}");
