824b30a32a30c902ae2e2c666744320440bec24a
EduardBakevich <ed.bakevich@gmail.com>
2023-06-21 12:35:54 +0300

    home4

---
+++ homeWorks/4/Program.cs
@@ -0,0 +1,64 @@
+﻿/* Цикл принимает на вход два числа (А и В) и возводит А в степень В*/
+
+// Console.Clear();
+
+// double AtoDegreeB(int a, int b)
+// {
+//     return Math.Pow (a, b);
+// }
+
+// System.Console.WriteLine ("Введите число A: " );
+// int a = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine ("Введите число B: " );
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// System.Console.WriteLine($" А в степени В => {AtoDegreeB(a, b)}");
+
+// /* Цикл принимает на вход число и выдает сумму цифр в числе*/
+// Console.Clear();
+
+// int HowMany (int n)
+// {
+//         int sum = 0;
+//     for (int i = 0; n >= 1; i++)
+//     {
+//         sum = sum + n%10;
+//         n = n / 10;
+//     }
+//     return sum;
+// }
+// Console.WriteLine("Введите число: " );
+// int n = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine($"Сумма цифр в числе: {HowMany (n)} ");
+
+
+
+/* Программа задает массив из Н элементов и выводит его на экран*/
+// Console.Clear();
+// int[] RandomArray (int size, int min, int max)
+// {
+//     int[] array = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         array[i] = new Random().Next(min, max + 1);
+//     }
+//     return array;
+// }
+
+// void Array(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         System.Console.Write(array[i] + " ");
+//     }
+//     System.Console.WriteLine();
+// }
+
+// System.Console.WriteLine("Введите желаемый размер массива: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите минимальное значение диапазона цифр: ");
+// int min = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите максимальное значение диапазона цифр: ");
+// int max = Convert.ToInt32(Console.ReadLine());
+
+// Array(RandomArray(size, min, max));
