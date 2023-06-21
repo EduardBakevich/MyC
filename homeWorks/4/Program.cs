f90eaa8a09ac0932329c975f0bc1d0ab389a3e46
EduardBakevich <ed.bakevich@gmail.com>
2023-06-21 17:41:45 +0300

    home4.1

--- homeWorks/4/Program.cs
+++ homeWorks/4/Program.cs
@@ -62,3 +62,37 @@
 // int max = Convert.ToInt32(Console.ReadLine());
 
 // Array(RandomArray(size, min, max));
+
+// int Prompt(string message)
+// {
+//     System.Console.WriteLine(message);
+//     string ReadInput = Console.ReadLine();
+//     int result = int.Parse(ReadInput);
+//     return result;
+// }
+// int[] GenerateArray(int Length, int minValue, int maxValue)
+// {
+//     int[] array = new int[Length];
+//     Random random = new Random();
+//     for (int i = 0; i < Length; i++)
+//     {
+//         array[i] = random.Next (minValue, maxValue + 1);
+//     }
+//     return array;
+// }
+// void PrintArray(int[] array)
+// {
+//     Console.Write(" [");
+//     for (int i = 0; i < array.Length - 1; i++)
+//     {
+//         Console.Write($"{array[i]}, ");
+//     }
+//     Console.WriteLine($"{array[array.Length - 1]}");
+//     Console.Write("]");
+// }
+
+// int length = Prompt ("Длина массива: ");
+// int min = Prompt ("Начальное значение, для диапазона случайного числа: ");
+// int max = Prompt ("Конечное значение, для диапазона случайного числа: ");
+// int[] array = GenerateArray(length, min, max);
+// PrintArray(array);
